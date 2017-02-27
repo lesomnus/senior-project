03602793877cfd24cad8823a5a6509a9daad16f8
MSP-SOMNUS\somnu <somnus950301@gmail.com>
2017-02-27 18:02:15 +0900

    fix  _receiver logic add open and close logic

--- client/DesktopStreamRcver.hpp
+++ client/DesktopStreamRcver.hpp
@@ -1,10 +1,14 @@
 #pragma once
 #include <cassert>
+#include <vector>
+#include <thread>
 
 #include <opencv2/opencv.hpp>
 #include <smns/io/sock.hpp>
 #include <smns/util/screen.hpp>
 #include <smns/DoubleBuffer.hpp>
+
+#include "DesktopStreamerCmd.h"
 
 class DesktopStreamRcver{
 private:
@@ -22,19 +26,31 @@ public:
 			  smns::io::Type::TCP){
 		_conn.addr(address);
 	}
-	~DesktopStreamRcver(){}
+	~DesktopStreamRcver(){ close(); }
 
 	bool is_open(){ return _is_open; }
 	bool is_close(){ return !is_open(); }
 	void open(){
 		if(_is_open) return;
 		_is_open = true;
+		_conn.connect();
+		_threads.push_back(
+			std::thread(&DesktopStreamRcver::_receiver, this));
+		_cmd_ready();
 	}
 	void close(){
 		if(!_is_open) return;
 		_is_open = false;
+
+		for(auto& thread : _threads)
+			thread.join();
+		_threads.clear();
+	}
+	Mat pop(){
+		return std::move(_shot_buff.pop());
 	}
 private:
+	std::vector<std::thread> _threads;
 	bool _is_open;
 	Sock _conn;
 	DBuf _shot_buff;
@@ -45,11 +61,12 @@ private:
 		Buff buff;
 		Mat encoded;
 		Mat decoded;
+		_shot_buff.open();
 		while(_is_open){
 			_conn.recv(buff, 4);
 			if(_conn.is_error()){};
 			const uint32_t size = *(reinterpret_cast<uint32_t*>(buff.data()));
-			const uint32_t thrsh_ready = size * 0.6; // TODO: make user definable.
+			const uint32_t thrsh_ready = size * 0.6; // TODO: make user definable. is it need?
 			uint32_t remain = size;
 			bool is_ready = false;
 
@@ -65,20 +82,30 @@ private:
 				}
 				remain -= chunk_s;
 				if(!is_ready && remain < thrsh_ready){
-					// TODO: send: ready to receive
+					_cmd_ready();
 					is_ready = true;
 				}
 				shot.insert(shot.end(), buff.begin(), buff.end());
 			}
 
-			shot.clear();
-			buff.clear();
-
 			encoded = cv::Mat(1, shot.size(), CV_8UC1, shot.data());
 			decoded = cv::imdecode(encoded, CV_LOAD_IMAGE_COLOR);
-			
 			// TODO: handle decode fail
+
+			shot.clear();
+			buff.clear();
+			
+			_shot_buff.push(std::move(decoded));
 		}
 	}
-	// TODO: command sender
+	void _send_cmd(DesktopStreamerCmd::cmd cmd){
+		static std::vector<uint8_t> buff;
+		buff.push_back(static_cast<uint8_t>(cmd));
+		_conn.send(buff);
+		buff.clear();
+	}
+	void _cmd_ready(){
+		_send_cmd(DesktopStreamerCmd::READY_TO_RCV);
+	}
+	// TODO: Size command
 };
