db21f429b902f2124cdb7dc3731f73499078faaa
MSP-SOMNUS\somnu <somnus950301@gmail.com>
2017-02-28 18:38:02 +0900

    add command: meta_size

--- server/DesktopStreamerCmd.h
+++ server/DesktopStreamerCmd.h
@@ -6,6 +6,10 @@ namespace DesktopStreamerCmd{
 enum cmd: uint8_t{
 	DISCONNECT,		// client disconnect
 	READY_TO_RCV,	// client ready to receive 
+	META_SIZE,		// metadata: client screen size
+					//	8-bit for code, 16-bit for width and height size[px]
+					//	total 8 + 16 + 16 = 40-bit(= 5-byte)
+					//	0x<code>'wwww'hhhh;
 	_SIZE			// number of commands
 };
 }
