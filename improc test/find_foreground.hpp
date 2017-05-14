9d531868b9c9816480eae06bc8c73c76afaee60b
MSP-SOMNUS\somnu <somnus950301@gmail.com>
2017-05-14 20:54:48 +0900

    update find_foreground

--- improc test/find_foreground.hpp
+++ improc test/find_foreground.hpp
@@ -3,46 +3,153 @@
 #include <cmath>
 #include <opencv2/opencv.hpp>
 
-//
-// ver 0.01
-//
+#include <string>
+#include <fstream>
+
+
+//	0	Edge feature
+//	1	RGB correlation in window
+//	2	Y correlation in window
+//	3	max(R,G,B)
+#define METHOD 2
+
+void writeCSV(std::string filename, cv::Mat m){
+	using namespace std;
+	ofstream myfile;
+	myfile.open(filename.c_str());
+	myfile << cv::format(m, cv::Formatter::FMT_CSV) << std::endl;
+	myfile.close();
+}
 
 cv::Mat find_foreground(const cv::Mat& src,
-						const cv::Mat& background){
+						const cv::Mat& background_){
 	using namespace cv;
-	enum BGR: uint8_t{ B, G, R };
-	const auto src_s = src.size();
+	constexpr bool BGR_BLURRED = true;
+	Mat rst;
+	Mat background = background_;
+	auto src_s = src.size();
+	if(BGR_BLURRED){
+		GaussianBlur(background, background, Size(5, 5), 1.5);
+		imshow("blurred bgr", background);
+	}
 
-	Mat diff(Size(src_s.width * 3, src_s.height), CV_8UC1);
-	Mat rst(src_s, CV_8UC1);
+	#if METHOD == 0			// Edge feature
+	Mat edg_src; Canny(src, edg_src, 10, 100);
+	Mat edg_bgr;
+	if(BGR_BLURRED) Canny(background, edg_bgr, 10, 100);
+	else Canny(background, edg_bgr, 100, 200);
 
-	for(auto y = 0; y < src_s.height; ++y){
-		for(auto x = 0; x < src_s.width; ++x){
-			const auto src_pix = src.at<Vec3b>(y, x);
-			const auto bgr_pix = background.at<Vec3b>(y, x);
-			for(auto i = 0; i < 3; ++i){
-				const auto dif = int(src_pix[i]) - int(bgr_pix[i]);
-				diff.at<uint8_t>(y, x * 3 + i)
-					= std::abs(dif) < 20 ? 255 : 0;
+	imshow("edge of src", edg_src);
+	imshow("edge of bgr", edg_bgr);
+
+	dilate(edg_bgr, edg_bgr, getStructuringElement(MORPH_RECT, {3, 3}));
+	rst = edg_src - edg_bgr;
+
+	#elif METHOD == 1		// RGB correlation in window
+	constexpr unsigned int WINDOW_W = 21;
+	constexpr unsigned int WINDOW_H = 21;
+	constexpr unsigned int WINDOW_N = WINDOW_W * WINDOW_H;
+	constexpr unsigned int PAD_X = WINDOW_W - 1; // padding of X-axis
+	constexpr unsigned int PAD_Y = WINDOW_H - 1; // padding of Y-axis
+	Mat srcf; src.convertTo(srcf, CV_32FC3);
+	Mat bgrf; bgrf.convertTo(bgrf, CV_32FC3);
+	Rect window(0, 0, WINDOW_W, WINDOW_H);
+	Mat corrl(Size((src_s.width - PAD_X) * 3,
+				   src_s.height - PAD_Y), CV_8UC1);
+	for(auto i = 0; i < 3; ++i){
+		Mat _src; extractChannel(src, _src, i);
+		Mat _bgr; extractChannel(background, _bgr, i);
+		for(auto y = 0; y < src_s.height - PAD_Y; ++y){
+			for(auto x = 0; x < src_s.width - PAD_X; ++x){
+				window.x = x; window.y = y;
+				Mat src_trg = _src(window);
+				Mat bgr_trg = _bgr(window);
+				Scalar src_m, src_std, bgr_m, bgr_std;
+				meanStdDev(src_trg, src_m, src_std);
+				meanStdDev(bgr_trg, bgr_m, bgr_std);
+				if(src_std[0] == 0) src_std[0] = 0.00001;
+				if(bgr_std[0] == 0) bgr_std[0] = 0.00001;
+				auto covar = (src_trg - src_m).dot(bgr_trg - bgr_m) / WINDOW_N;
+				if(covar == 0) covar = 0.00001;
+				auto correl = covar / (src_std[0] * bgr_std[0]);
+				//std::cout << covar << std::endl;
+				corrl.at<uint8_t>(y, x * 3 + i) = (correl > 1 ? 1 : correl) * 255;
 			}
 		}
 	}
-	
+	rst = corrl < (255 / 8);
 
-	for(auto y = 0; y < src_s.height; ++y){
-		for(auto x = 0; x < src_s.width; ++x){
-			auto sum = 0;
-			for(auto i = 0; i < 3; ++i){
-				sum += diff.at<uint8_t>(y, x * 3 + i);
-			}
-			rst.at<uint8_t>(y, x) = sum / 3;
+	#elif METHOD == 2		// Y correlation in window
+	constexpr unsigned int WINDOW_W = 21;
+	constexpr unsigned int WINDOW_H = 21;
+	constexpr unsigned int WINDOW_N = WINDOW_W * WINDOW_H;
+	constexpr unsigned int PAD_X = WINDOW_W - 1; // padding of X-axis
+	constexpr unsigned int PAD_Y = WINDOW_H - 1; // padding of Y-axis
+	Mat src_ycc; cvtColor(src, src_ycc, CV_BGR2YCrCb);
+	Mat bgr_ycc; cvtColor(background, bgr_ycc, CV_BGR2YCrCb);
+	Rect window(0, 0, WINDOW_W, WINDOW_H);
+	Mat corrl(Size((src_s.width - PAD_X - 1),
+				   src_s.height - PAD_Y - 1), CV_8UC1);
+
+	Mat _src; extractChannel(src_ycc, _src, 0);
+	Mat _bgr; extractChannel(bgr_ycc, _bgr, 0);
+
+	for(auto y = 0; y < src_s.height - PAD_Y; ++y){
+		for(auto x = 0; x < src_s.width - PAD_X; ++x){
+			window.x = x; window.y = y;
+			Mat src_trg = _src(window);
+			Mat bgr_trg = _bgr(window);
+			Scalar src_m, src_std, bgr_m, bgr_std;
+			meanStdDev(src_trg, src_m, src_std);
+			meanStdDev(bgr_trg, bgr_m, bgr_std);
+			if(src_std[0] == 0) src_std[0] = 0.00001;
+			if(bgr_std[0] == 0) bgr_std[0] = 0.00001;
+			auto covar = (src_trg - src_m).dot(bgr_trg - bgr_m) / WINDOW_N;
+			if(covar == 0) covar = 0.00001;
+			auto correl = covar / (src_std[0] * bgr_std[0]);
+			//std::cout << covar << std::endl;
+			corrl.at<uint8_t>(y, x) = (correl > 1 ? 1 : correl) * 255;
+		}
+	}
+	rst = corrl < (255 / 6);
+
+	#elif METHOD == 3
+	rst = Mat(src_s, CV_8UC1);
+	constexpr unsigned int WINDOW_W = 21;
+	constexpr unsigned int WINDOW_H = 21;
+	constexpr unsigned int WINDOW_N = WINDOW_W * WINDOW_H;
+	constexpr unsigned int PAD_X = WINDOW_W - 1; // padding of X-axis
+	constexpr unsigned int PAD_Y = WINDOW_H - 1; // padding of Y-axis
+	Rect window(0, 0, WINDOW_W, WINDOW_H);
+	Mat _rst(Size((src_s.width - PAD_X - 1),
+				   src_s.height - PAD_Y - 1), CV_8UC1);
+
+	auto max_index = [](const Scalar& src){
+		if(src[0] > src[1] && src[0] > src[2]){
+			return 0;
+		} else if(src[1] > src[0] && src[1] > src[2]) return 1;
+		else return 2;
+	};
+
+	for(auto y = 0; y < src_s.height - PAD_Y; ++y){
+		for(auto x = 0; x < src_s.width - PAD_X; ++x){
+			window.x = x; window.y = y;
+			Mat src_trg = src(window);
+			Mat bgr_trg = background(window);
+			auto src_trg_sum = sum(src_trg);
+			auto bgr_trg_sum = sum(bgr_trg);
+			if(max_index(src_trg_sum) == max_index(bgr_trg_sum)){
+				_rst.at<uint8_t>(y, x) = 255;
+			} else _rst.at<uint8_t>(y, x) = 0;
 		}
 	}
 
-	{ // open
-		erode(rst, rst, Mat());
-		dilate(rst, rst, Mat());
-	}
+	rst = _rst;
+
+	#endif
+
 
 	return rst;
 }
+
+#undef METHOD
--- improc test/main.cpp
+++ improc test/main.cpp
@@ -1,7 +1,7 @@
 #if defined(_DEBUG)
 #	define IFNDBG(expr) {}
 #else
-#	define IFNDBG(expr) expr
+#	define IFNDBG(expr) {} //expr
 #endif
 
 #include <iostream>
@@ -16,143 +16,83 @@
 #include "find_foreground.hpp"
 
 int main(){
-	constexpr size_t DFT_SRC_BRIGHTNESS_LEVEL = 0;
 	constexpr char* RESOURCE_PATH = "./resource/";
+	constexpr bool IS_WHITE = false;
 	using namespace std;
 	using namespace cv;
-	size_t src_brightness_level = 0;
-	
-	auto get_resource_name = [&src_brightness_level, RESOURCE_PATH](const char* type){
+
+	auto get_resource_name = [RESOURCE_PATH](const char* filename){
 		string name;
 		name += RESOURCE_PATH;
-		name += type;
-		name += to_string(src_brightness_level);
+		name += filename;
 		name += ".png";
 		return name;
 	};
 
-	Mat src;
-	{
-		cout << "Choose brightness level to test between 1 and 3, other to default. \n"
-			<< "The smaller the number, the darker it is." << endl;
-
-		cin >> src_brightness_level;
-		cin.clear();
-
-		if(src_brightness_level > 3 || src_brightness_level == 0)
-			src_brightness_level = DFT_SRC_BRIGHTNESS_LEVEL;
-
-		string filename = get_resource_name("pattern");
-		src = imread(filename);
-		if(!src.data){
-			cout << "Unable to load. -> " << filename << endl;
-			return -1;
-		}
-	}
-
 	//
 	// source pattern image
 	//
-	imshow("src", src);
+	Mat src_pattern = imread(get_resource_name("pattern"));
+	imshow("src pattern", src_pattern);
 	IFNDBG(waitKey(0));
 
 	//
 	// area found
 	//
-	Mat projected_area = find_projected_area(src);
+	Mat projected_area = find_projected_area(src_pattern);
 	imshow("projected area", projected_area);
 	IFNDBG(waitKey(0));
 
 	//
-	// false corner found
+	// corners found
 	//
+	using Projector = PerspectiveProjector<Vec3b>;
 	using Corners = std::array<Point, 4>;
-	Mat corner_found = src.clone();
-	Corners false_corners = find_corners(projected_area);
-	for(const auto& corner : false_corners){
-		circle(corner_found, corner, 3, Scalar(0, 0, 255), -1);
-	}
-	imshow("corner found", corner_found);
-	IFNDBG(waitKey(0));
-
-	//
-	// true coner found
-	// using perspective projection
-	//
-	using Projector = PerspectiveProjector<cv::Vec3b>;
-	Projector back_projector(false_corners);
-	Corners true_corners;
+	Projector back_projector;
+	Corners corners;
+	Size bound_s;
 	{
-		auto bound = back_projector.bound();
-		true_corners = scale_from_center({
-			Point(0,0), Point(bound.width, 0), Point(0, bound.height),
-			Point(bound.width, bound.height)
-		}, 2);
-		for(auto& corner : true_corners){
-			corner = back_projector(corner);
+		Corners false_corners = find_corners(projected_area);
+		for(const auto& corner : false_corners){
+			circle(src_pattern, corner, 3, Scalar(0, 0, 255), -1);
 		}
+		back_projector = Projector(false_corners);
+		auto bound = back_projector.bound();
+		bound_s = bound.size();
+		corners = scale_from_center({
+					Point(0,0), Point(bound.width, 0), Point(0, bound.height),
+					Point(bound.width, bound.height)
+		}, 4.0 / 3 - 0.0125); // 4 / 3;
+		for(auto& corner : corners){
+			corner = back_projector(corner);
+			circle(src_pattern, corner, 3, Scalar(255, 0, 0), -1);
+		}
+		back_projector = Projector(corners);
 	}
-	for(const auto& corner : true_corners){
-		circle(corner_found, corner, 3, Scalar(255, 0, 0), -1);
-	}
-	imshow("corner found", corner_found);
+	imshow("src pattern", src_pattern);
+	IFNDBG(waitKey(0));
+	
+	//
+	// original (background)
+	//
+	Mat org = imread(get_resource_name(IS_WHITE ? "orgwhite" : "org"));
+	resize(org, org, bound_s);
+	imshow("original", org);
 	IFNDBG(waitKey(0));
 
 	//
-	// source shot
+	// shot
 	//
-	Mat src_shot = imread(get_resource_name("shot"));
-	imshow("shot", src_shot);
+	Mat shot = imread(get_resource_name(IS_WHITE ? "forewhite" : "shot_fore"));
+	shot = back_projector(shot);
+	resize(shot, shot, bound_s);
+	imshow("shot", shot);
 	IFNDBG(waitKey(0));
-
-	//
-	// project source shot
-	//
-	back_projector.eval(true_corners);
-	Mat projected = back_projector(src_shot);
-	imshow("projected shot", projected);
-	IFNDBG(waitKey(0));
-
-	//
-	// source fore shot
-	//
-	Mat src_fore_shot = imread(get_resource_name("fore"));
-	imshow("fore shot", src_fore_shot);
-	IFNDBG(waitKey(0));
-
-	//
-	// project source fore shot
-	//
-	Mat fore_projected = back_projector(src_fore_shot);
-	imshow("projected fore shot", fore_projected);
-	IFNDBG(waitKey(0));
-
-	//
-	// destroy not necessary windows
-	//
-	destroyWindow("src");
-	destroyWindow("projected area");
-	destroyWindow("shot");
-	destroyWindow("projected shot");
-
-	//
-	// original shot
-	//
-	Mat org_shot = imread(get_resource_name("org"));
-	resize(org_shot, org_shot, org_shot.size() / 4);
-	imshow("original shot", org_shot);
-	IFNDBG(waitKey(0));
-
-	//
-	// resize projected fore shot to org's size
-	//
-	resize(fore_projected, fore_projected, org_shot.size());
-	imshow("projected fore shot", fore_projected);
 
 	//
 	// find foreground
 	//
-	Mat foreground = find_foreground(fore_projected, org_shot);
+	Mat foreground = find_foreground(shot, org);
 	//normalize(foreground, foreground, 0, 255, cv::NORM_MINMAX);
 	imshow("foreground", foreground);
 
